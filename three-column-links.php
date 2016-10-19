154f98591bf26010b11f1d8e91ecfddb1e01f62f
duhaime <douglas.duhaime@gmail.com>
2016-10-19 20:50:32 +0000

    add home page partial

---
+++ three-column-links.php
@@ -0,0 +1,74 @@
+<div class="three-column-links">
+  <div class="row sleek-animate-appearance" style="padding-top:10px;padding-bottom:20px;">
+    <div class="row__inwrap">
+      <div class="column-wrap">
+        <p style="text-align: center;"></p>
+        
+        <!-- Link to Reading -->
+        <div class="column--1-3 home-image-link">
+          <div class="aligncenter wp-image-3258 size-full home-image home-image-one" height="63"
+          width="60"></div>
+          <a href="./index.php/category/reading/">
+            <span class="custom-heading" style="text-align: center;">READING</span>
+            <p></p>
+            <p style="text-align: center;">Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit, sed do eiusmod et dolore magna
+            aliqua.</p>
+            <p style="text-align: center;"></p>
+          </a>
+        </div>
+
+        <!-- Annotating -->
+        <div class="column--1-3 home-image-link">
+          <div alt="" class="aligncenter wp-image-3257 size-full home-image home-image-two" height="63"
+              width="60"></div>
+          <a href="./index.php/category/annotating/">
+            <span class="custom-heading" style="text-align: center;">ANNOTATING</span>
+            <p></p>
+            <p style="text-align: center;">Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit, sed do eiusmod et dolore magna
+            aliqua.</p>
+            <p style="text-align: center;"></p>
+          </a>
+        </div>
+
+        <!-- Remixing -->
+        <div class="column--1-3 home-image-link">
+          <div alt="" class="aligncenter wp-image-3253 size-full home-image home-image-three" height="63"
+            width="60"></div>
+          <a href="./index.php/category/remixing">
+            <span class="custom-heading" style="text-align: center;">REMIXING</span>
+            <p></p>
+            <p style="text-align: center;">Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit, sed do eiusmod et dolore magna
+            aliqua.</p>
+            <p style="text-align: center;"></p>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <p style="text-align: center;"></p>
+  <div class="row" style=
+  "padding-top:30px;padding-bottom:40px;background:#f5f5f5;">
+    <div class="row__inwrap">
+      <div class="column-wrap">
+        <div class="column--1-1 sleek-animate-appearance">
+          <p style="text-align: center;"></p>
+          <h2 style="text-align: center;">THESE ARE THEIR STORIES</h2>
+          <p style="text-align: center;"><span class=
+          "separator separator--medium separator--center" style=
+          ""></span></p>
+          <p style="text-align: center;">Ut wisi enim ad minim
+          veniam, quis nostrud exerci tation ullamcorper suscipit
+          lobortis nisl ut aliquip ex<br>
+          ea commodo consequat uis autem vel eum iriure dolor in
+          hendrerit in vulputate<br>
+          Lorem ipsum dolor sit amet consectetuer <a href="#" title=
+          "Twitter">Follow</a> adipiscing elit</p>
+          <p style="text-align: center;"></p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
